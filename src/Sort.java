bb8ef39cef2c05f821b60708e314cdcc9012b092
rexdarel <rexdarel.andig@g.msuiit.edu.ph>
2017-10-09 22:02:55 +0800

    Added reading integers from a file and running time of the Sorting algos

--- src/Sort.java
+++ src/Sort.java
@@ -1,16 +1,45 @@
+import java.awt.*;
+import java.io.*;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Sort {
 
-    public static void main(String[] args){
-        int[] array = {1,23,3,32,56,};
+    public static void main(String[] args) throws IOException {
+        
+        int array[] = new int[15000];
+        int i = 0;
+        FileInputStream fstream = new FileInputStream("random15000.txt");
+        DataInputStream in = new DataInputStream(fstream);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String data;
+        while ((data = br.readLine()) != null)   {
+            String[] tmp = data.split(" ");    //Split space
+            for(String s: tmp)
+                array[i++] = Integer.parseInt(s);
+        }
 
         BubbleSort bS = new BubbleSort();
         SelectionSort sS = new SelectionSort();
         InsertionSort iS = new InsertionSort();
 
-        System.out.println("Bubble Sort: " + Arrays.toString(bS.bubbleSort(array)));
-        System.out.println("Selection Sort: " + Arrays.toString(sS.selectionSort(array)));
-        System.out.println("Insertion Sort: " + Arrays.toString(iS.insertionSort(array)));
+        long startTimeBS = System.nanoTime();
+        bS.bubbleSort(array);
+        long endTimeBS = System.nanoTime();
+        long totalTimeBS = endTimeBS - startTimeBS;
+
+        long startTimeSS = System.nanoTime();
+        sS.selectionSort(array);
+        long endTimeSS = System.nanoTime();
+        long totalTimeSS = endTimeSS - startTimeSS;
+
+        long startTimeIS = System.nanoTime();
+        iS.insertionSort(array);
+        long endTimeIS = System.nanoTime();
+        long totalTimeIS = endTimeIS - startTimeIS;
+
+        System.out.println("Running time of Bubble Sort: " + totalTimeBS);
+        System.out.println("Running time of Selection Sort: " + totalTimeSS);
+        System.out.println("Running time of Insertion Sort: " + totalTimeIS);
     }
 }
